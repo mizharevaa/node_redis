d00d4ec56f0551f916c934e2e24b6ca801c3be41
mizharevaa <mizharevaa@mail.ru>
2022-06-21 15:47:50 +0400

    redis

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import  express from 'express';
+import  {express} from 'express';
+import {Redis} from 'ioredis';
+
+const redis = new Redis()
+
+async function Redis_work(in_str){
+    return await redis.ping(in_str)
+}
 
 const PORT = '5000'
 const app = express()
@@ -6,7 +13,14 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res)=>{
-    res.status(200).json('это json, честно!')
+    // res.status(200).json('это json, честно!')
+    const answer = Redis_work(req.query.echo)
+    if(answer === req.query.echo) {
+        res.status(200).json(answer)
+    } else {
+        res.status(500).json('Что-то не так с редисом!')
+    }
+    
 })
 
 app.listen(PORT, () => console.log('Сервер работает!'))
