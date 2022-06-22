d97c1b560c6149177778bb8cbc40dc21cfd35b73
mizharevaa <mizharevaa@mail.ru>
2022-06-22 09:42:23 +0400

    redis16

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import  express from 'express';
-import { createClient } from 'redis';
+import {createClient} from 'redis';
 
-const redis = new createClient();
-await redis.connect();
+const client = createClient();
+await client.connect();
 
-async function Redis_work(in_str){
+
+async function Redis_work(){
     // const [res, val] = await redis.ping(in_str)
     // console.log(res)
 
-    await redis.sendCommand(['geoadd','Circle','13.361389', '38.115556', 'Palermo', '15.087269', '37.502669', 'Catania'])
-    const val = await redis.sendCommand(['georadius', 'Circle','15', '37', 200, 'km', 'WITHCOORD'])
-    console.log(val)
+    await client.sendCommand(['geoadd','Circle','13.361389', '38.115556', 'Palermo', '15.087269', '37.502669', 'Catania'])
+    const [res, val] = await client.sendCommand(['georadius', 'Circle','15', '37', 200, 'km', 'WITHCOORD'])
+    console.log(res)
 
     return val
 }
@@ -22,13 +23,19 @@ app.use(express.json())
 
 app.get('/', async (req, res)=>{
    
-    const answer = await Redis_work(req.query.echo)
-    if(answer === req.query.echo) {
-        res.status(200).json(answer)
-    } else {
-        res.status(500).json(answer)
-    }
+    // const answer = await Redis_work(req.query.echo)
+    // if(answer === req.query.echo) {
+    //     res.status(200).json(answer)
+    // } else {
+    //     res.status(500).json(answer)
+    // }
     
+    const answer = await Redis_work()
+    // if(answer === req.query.echo) {
+    //     res.status(200).json(answer)
+    // } else {
+        res.status(500).json(answer)
+    // }
 })
 
 app.listen(PORT, () => console.log('Сервер работает!'))
