6a23955e6f32c8e4d1bcb6a80f9a5b394b2f2df2
mizharevaa <mizharevaa@mail.ru>
2022-06-24 09:19:06 +0400

    redis33

--- index.js
+++ index.js
@@ -4,20 +4,15 @@ import {createClient} from 'redis';
 const client = createClient();
 await client.connect();
 
-
 async function Redis_work(in_str, query_str){
-    // const [res, val] = await redis.ping(in_str)
-    // console.log(res)
+    
     await client.flushAll()
-    // const res1 = await client.sendCommand(['GEOADD', 'Circle', '13.361389', '38.115556', 'Palermo', '15.087269', '37.502669', 'Catania'])
-    // const [res2, val] = await client.sendCommand(['GEORADIUS', 'Circle', '15', '37', '200', 'km', 'WITHCOORD'])
+    // await client.sendCommand(['GEOADD', 'Circle', '13.361389', '38.115556', 'Palermo', '15.087269', '37.502669', 'Catania'])
+    // val = await client.sendCommand(['GEORADIUS', 'Circle', '15', '37', '200', 'km', 'WITHCOORD'])
 
-    const res1 = await client.sendCommand(in_str)
-    const [res2, val] = await client.sendCommand(query_str)
+    await client.sendCommand(in_str)
+    val = await client.sendCommand(query_str)
 
-    console.log(res1)
-    console.log(res2)
-    console.log(val)
     return val
 }
 
@@ -28,22 +23,15 @@ app.use(express.json())
 
 app.post('/', async (req, res)=>{
     const in_str = req.body.in_str.split(', ')
-    console.log(in_str)
     const query_str = req.body.query_str.split(', ')
-    console.log(query_str)
-    // const answer = await Redis_work(req.query.echo)
-    // if(answer === req.query.echo) {
-    //     res.status(200).json(answer)
-    // } else {
-    //     res.status(500).json(answer)
-    // }
-    
-    const answer = await Redis_work(in_str, query_str)
-    // if(answer === req.query.echo) {
-    //     res.status(200).json(answer)
-    // } else {
-        res.status(500).json(answer)
-    // }
+
+    try {
+        const answer = await Redis_work(in_str, query_str)
+        res.status(200).json(answer)
+    } catch (error) {
+        res.status(500).write('Somthing went wrong: '+ error)
+    }
+   
 })
 
 app.listen(PORT, () => console.log('Сервер работает!'))
